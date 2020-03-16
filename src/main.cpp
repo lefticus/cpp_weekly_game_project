5fee99e9f8379a7bd643594ac8f6588c73d9028f
Jason Turner <jason@emptycrate.com>
2020-03-16 15:02:27 -0600

    Add command line argument handling with docopt

--- src/main.cpp
+++ src/main.cpp
@@ -11,51 +11,66 @@
 
 #include <docopt/docopt.h>
 
-/*
 static constexpr auto USAGE =
-  R"(Naval Fate.
-
+  R"(C++ Weekly Game.
     Usage:
-          naval_fate ship new <name>...
-          naval_fate ship <name> move <x> <y> [--speed=<kn>]
-          naval_fate ship shoot <x> <y>
-          naval_fate mine (set|remove) <x> <y> [--moored | --drifting]
-          naval_fate (-h | --help)
-          naval_fate --version
- Options:
-          -h --help     Show this screen.
-          --version     Show version.
-          --speed=<kn>  Speed in knots [default: 10].
-          --moored      Moored (anchored) mine.
-          --drifting    Drifting mine.
+          game [options]
+
+  Options:
+          -h --help         Show this screen.
+          --width=WIDTH     Screen width in pixels [default: 1024].
+          --height=HEIGHT   Screen height in pixels [default: 768].
+          --scale=SCALE     Scaling factor [default: 2].
 )";
-*/
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] const char **argv)
+int main(int argc, const char **argv)
 {
-  //  std::map<std::string, docopt::value> args = docopt::docopt(USAGE,
-  //    { std::next(argv), std::next(argv, argc) },
-  //    true,// show help if requested
-  //    "Naval Fate 2.0");// version string
+  std::map<std::string, docopt::value> args = docopt::docopt(USAGE,
+    { std::next(argv), std::next(argv, argc) },
+    true,// show help if requested
+    "Game 0.0");// version string
 
-  //  for (auto const &arg : args) {
-  //    std::cout << arg.first << arg.second << std::endl;
-  //  }
+  const auto width = args["--width"].asLong();
+  const auto height = args["--height"].asLong();
+  const auto scale = args["--scale"].asLong();
+
+  if (width < 0 || height < 0 || scale < 1 || scale > 5) {
+    spdlog::error("Command line options are out of reasonable range.");
+    for (auto const &arg : args) {
+      if (arg.second.isString()) { spdlog::info("Parameter set: {}='{}'", arg.first, arg.second.asString()); }
+    }
+    abort();
+  }
 
 
-  //Use the default logger (stdout, multi-threaded, colored)
+  // Use the default logger (stdout, multi-threaded, colored)
   spdlog::info("Hello, {}!", "World");
 
 
-  sf::RenderWindow window(sf::VideoMode(1024, 768), "ImGui + SFML = <3");
+  sf::RenderWindow window(
+    sf::VideoMode(static_cast<unsigned int>(width), static_cast<unsigned int>(height)), "ImGui + SFML = <3");
   window.setFramerateLimit(60);
   ImGui::SFML::Init(window);
 
-  constexpr auto scale_factor = 2.0;
+  const auto scale_factor = static_cast<float>(scale);
   ImGui::GetStyle().ScaleAllSizes(scale_factor);
   ImGui::GetIO().FontGlobalScale = scale_factor;
 
-  std::array<bool, 12> states{};
+  constexpr std::array steps = { "The Plan",
+    "Getting Started",
+    "Finding Errors As Soon As Possible",
+    "Handling Command Line Parameters",
+    "C++ 20 So Far",
+    "Reading SFML Input States",
+    "Managing Game State",
+    "Making Our Game Testable",
+    "Making Game State Allocator Aware",
+    "Add Logging To Game Engine",
+    "Draw A Game Map",
+    "Dialog Trees",
+    "Porting From SFML To SDL" };
+
+  std::array<bool, steps.size()> states{};
 
   sf::Clock deltaClock;
   while (window.isOpen()) {
@@ -63,9 +78,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] const char **argv)
     while (window.pollEvent(event)) {
       ImGui::SFML::ProcessEvent(event);
 
-      if (event.type == sf::Event::Closed) {
-        window.close();
-      }
+      if (event.type == sf::Event::Closed) { window.close(); }
     }
 
     ImGui::SFML::Update(window, deltaClock.restart());
@@ -73,9 +86,8 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] const char **argv)
 
     ImGui::Begin("The Plan");
 
-    int index = 0;
-    for (const auto &step : { "The Plan", "Getting Started", "Finding Errors As Soon As Possible", "Handling Command Line Parameters", "C++ 20 So Far", "Reading SFML Input States", "Managing Game State", "Making Our Game Testable", "Making Game State Allocator Aware", "Add Logging To Game Engine", "Draw A Game Map", "Dialog Trees", "Porting From SFML To SDL" }) {
-      ImGui::Checkbox(fmt::format("{} : {}", index, step).c_str(), std::next(begin(states), index));
+    for (std::size_t index = 0; const auto &step : steps) {
+      ImGui::Checkbox(fmt::format("{} : {}", index, step).c_str(), &states.at(index));
       ++index;
     }
 
